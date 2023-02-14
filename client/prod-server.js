23a49a3af969795fbb1d2401da9fb13ea077648d
Zharkeyev <zharkeev.post@gmail.com>
2023-02-14 22:17:05 +0600

    development add keys

--- client/prod-server.js
+++ client/prod-server.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const path = require('path')
 const dotenv = require('dotenv')
-
+const https = require('https')
 const ENV = dotenv.config({ path: './_env/.env.production' }).parsed
 const app = express()
 
-app.listen(ENV.CLIENT_PORT)
-
 app.use(express.static(path.join(__dirname + '/bundle/')))
 
-/** app.get(*) - doesn't work!!! */
+const server = https.createServer(app)
+server.listen(ENV.CLIENT_PORT)
+
 const routes = ['/', '/app', '/app/', '/sign-in', '/sign-up', '/wait-email-confirm', '/confirm-email']
 routes.forEach(route =>{
   app.get(route, (_, res) => res.sendFile(path.join(__dirname, './bundle/index.html')))
--- server/src/server.ts
+++ server/src/server.ts
@@ -3,33 +3,40 @@ import { Server } from 'socket.io'
 import router from './router'
 import ENV from './ENV'
 const http = require('http')
+const https = require('https')
 const methodOverride = require('method-override')
 const bodyParser = require('body-parser')
 const clc = require('cli-color')
 const cookieParser = require('cookie-parser')
-
+const fs = require('express')
 const app = express()
-const server = http.createServer(app)
-
-const { ExpressPeerServer } = require('peer')
-const peerServer = ExpressPeerServer(server, {
-  debug: true
-})
-app.use('/peerjs', peerServer)
 
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(methodOverride('_method'))
 app.use('/', router)
 
+app.get('/', (req: Request, res: Response) => {
+  res.send('Server running')
+})
+
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/k-room.space/privkey.pem', 'utf8')
+const certificate = fs.readFileSync('/etc/letsencrypt/live/k-room.space/cert.pem', 'utf8')
+const ca = fs.readFileSync('/etc/letsencrypt/live/k-room.space/chain.pem', 'utf8')
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+}
+
+const server = ENV.IS_DEV ? http.createServer(app) : https.createServer(credentials, app)
+
 const PORT = ENV.SERVER_PORT
 
 server.listen(PORT, () => {
   console.log(clc.green.bgWhite(`-Server listening on port ${PORT}`))
-})
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Server running')
 })
 
 export const io = new Server(server, {
