5b2c6f77260402bd4f069b5cb3a7c659a72e9145
Zharkeyev <zharkeev.post@gmail.com>
2023-02-18 12:03:57 +0600

    fix config

--- client/prod-server.js
+++ client/prod-server.js
@@ -2,16 +2,16 @@ const express = require('express')
 const path = require('path')
 const dotenv = require('dotenv')
 const https = require('https')
-const http = require('http')
+// const http = require('http')
 const ENV = dotenv.config({ path: './_env/.env.production' }).parsed
 const app = express()
 
 app.use(express.static(path.join(__dirname + '/bundle/')))
 
-const httpServer = http.createServer(app)
+// const httpServer = http.createServer(app)
 const httpsServer = https.createServer(app)
 
-httpServer.listen(ENV.CLIENT_PORT)
+// httpServer.listen(ENV.CLIENT_PORT)
 httpsServer.listen(ENV.CLIENT_PORT)
 
 const routes = ['/', '/app', '/app/', '/sign-in', '/sign-up', '/wait-email-confirm', '/confirm-email']
