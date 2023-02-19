18c8eb1499a33c1375b20baeba95cda5d21702d8
Zharkeyev <zharkeev.post@gmail.com>
2023-02-19 14:18:53 +0600

    fix paths

--- types/index.ts
+++ types/index.ts
@@ -2,6 +2,7 @@ export interface EnvVariables {
   SERVER_PORT: string;
   CLIENT_PORT: string;
   SERVER_URL: string;
+  CLIENT_URL: string;
   MONGO_HOST: string;
   HOST: string;
   JWT_ACCESS_EXPIRES_INTERVAL: string;
