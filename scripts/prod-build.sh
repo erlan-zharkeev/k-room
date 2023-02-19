9fc797a73ea1859e7ebbbaf4d85da8ebede74ef2
Zharkeyev <zharkeev.post@gmail.com>
2023-02-19 12:54:27 +0600

    fix config

--- scripts/prod-build.sh
+++ scripts/prod-build.sh
@@ -2,9 +2,8 @@
 
 cd ..
 git restore .
-# git checkout development-enable-https
 git pull
 cd scripts
 source update-envs.sh
-# docker image prune --filter="dangling=true" -f
-docker-compose --env-file .env.production up -d --build webserver
+docker image prune --filter="dangling=true" -f
+docker-compose --env-file .env.production up --build
