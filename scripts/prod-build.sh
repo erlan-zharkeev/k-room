3737636f42e4dc66b4e620647472416c12a74c86
Zharkeyev <zharkeev.post@gmail.com>
2023-02-19 15:19:12 +0600

    fix image appearance

--- scripts/prod-build.sh
+++ scripts/prod-build.sh
@@ -6,4 +6,4 @@ git pull
 cd scripts
 source update-envs.sh
 docker image prune --filter="dangling=true" -f
-docker-compose --env-file .env.production up --build
+docker-compose --env-file .env.production up -d --build
