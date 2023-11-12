3ad1b23c021dce1b336297943c8085ad643472d2
Zharkeyev <zharkeev.post@gmail.com>
2023-11-12 14:51:32 +0600

    fix glob vars

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -9,7 +9,9 @@ import { RefsProvider } from 'src/providers/RefsProvider'
 import { AdditionalServiceProvider } from './providers/AdditionalServiceProvider'
 import { initializeApp } from 'firebase/app'
 import * as process from 'process'
-console.log(import.meta.env)
+
+console.log(import.meta.env, 'env')
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: 'k-room-3a49a.firebaseapp.com',
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
+  console.log(env)
   const VITE_FIREBASE_API_KEY = JSON.stringify(env.VITE_FIREBASE_API_KEY)
   return {
     define: {
