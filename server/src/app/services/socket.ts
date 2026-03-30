449fcb2a7d52580ccb52ffe910bdb8a2458d41b1
Erlan <zharkeev.post@gmail.com>
2026-03-30 14:35:09 +0700

    fix(server): use http backend outside development

--- server/src/app/services/socket.ts
+++ server/src/app/services/socket.ts
@@ -1,3 +1,4 @@
+import type { Server as HttpServer } from 'http'
 import type { Server as HttpsServer } from 'https'
 import { Server } from 'socket.io'
 
@@ -11,7 +12,9 @@ import { socketAuthMiddleware } from 'src/features/auth'
 import { ENV, ORIGINS, type SocketInstanceType } from 'src/shared/config'
 import { log, serverCaptureSentryException } from 'src/shared/lib'
 
-const getSocketIO = (server: HttpsServer) =>
+type SocketServerType = HttpServer | HttpsServer
+
+const getSocketIO = (server: SocketServerType) =>
   new Server(server, {
     path: RouteNamesEnum.SocketPath,
     maxHttpBufferSize: MAX_HTTP_BUFFER_SIZE,
@@ -21,7 +24,7 @@ const getSocketIO = (server: HttpsServer) =>
     }
   })
 
-export const initIO = (server: HttpsServer): Server => {
+export const initIO = (server: SocketServerType): Server => {
   const io = getSocketIO(server)
   try {
     io.on<SocketActionsType>('connection', async (socket: SocketInstanceType) => {
