e70cd444e5a5ee32c752129c9735869b804038bd
Erlan <zharkeev.post@gmail.com>
2026-04-03 09:52:44 +0700

    fix: restore socket actualize flow and docker client build

--- server/src/app/services/socket-router/user.ts
+++ server/src/app/services/socket-router/user.ts
@@ -1,8 +1,8 @@
-import { updateLanguageController, userConnectController, userDisconnectController } from 'src/features/user'
+import { actualizeUserDataController, updateLanguageController, userConnectController, userDisconnectController } from 'src/features/user'
 
 import { SocketInstanceType } from 'src/shared/config'
 
-const controllers = [userConnectController, userDisconnectController, updateLanguageController]
+const controllers = [userConnectController, userDisconnectController, updateLanguageController, actualizeUserDataController]
 
 export const socketUserRouter = (socket: SocketInstanceType) => {
   controllers.forEach((controller) => controller(socket))
