fc734c8fdce636f29b3669e236fa0a8aee10abcb
Erlan <zharkeev.post@gmail.com>
2026-04-04 10:43:38 +0700

    feat: add connect-mongo for persistent admin sessions

--- server/src/app/services/admin.ts
+++ server/src/app/services/admin.ts
@@ -1,6 +1,7 @@
 import AdminJSExpress from '@adminjs/express'
 import { Database, Resource } from '@adminjs/mongoose'
 import AdminJS from 'adminjs'
+import MongoStore from 'connect-mongo'
 
 import { ADMIN_INFO_NOTIFICATION_OPTIONS } from 'src/features/info-notification'
 
@@ -38,11 +39,16 @@ export const adminRouter = AdminJSExpress.buildAuthenticatedRouter(
   {
     secret: SERVER_ENV.refreshTokenSecret,
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
     name: SERVER_ENV.adminCookie,
+    store: MongoStore.create({
+      mongoUrl: SERVER_ENV.mongoHost,
+      collectionName: 'admin-session'
+    }),
     cookie: {
       httpOnly: true,
       secure: !SERVER_ENV.isDev,
+      sameSite: 'lax',
       maxAge: 7 * 24 * 60 * 60 * 1000
     }
   }
