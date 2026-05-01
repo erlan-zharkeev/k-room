5ae4a4e0fd3d20cd121830b10a0c5c11c1b3c369
Erlan <zharkeev.post@gmail.com>
2026-05-01 22:09:13 +0500

    development

--- e2e/config/constants.ts
+++ e2e/config/constants.ts
@@ -1,10 +1,12 @@
 const NODE_ENV = process.env.NODE_ENV ?? 'development'
+const PLAYWRIGHT_API_URL = 'https://localhost:43117/api'
 
 export const E2E_ENV = {
   NODE_ENV,
   IS_DEV: NODE_ENV === 'development',
+  PLAYWRIGHT_API_URL,
   PLAYWRIGHT_BASE_URL: 'https://localhost:43111',
-  PLAYWRIGHT_SERVER_URL: 'https://localhost:43117/api/users/me',
+  PLAYWRIGHT_SERVER_URL: `${PLAYWRIGHT_API_URL}/users/me`,
   PLAYWRIGHT_MONGO_HOST: '127.0.0.1',
   PLAYWRIGHT_MONGO_PORT: 27017,
   CI: Boolean(process.env.CI)
