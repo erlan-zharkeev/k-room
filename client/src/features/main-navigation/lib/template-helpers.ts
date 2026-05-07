76473085cd5ac65ddd27aeebe382d4443fe2c77e
Erlan <zharkeev.post@gmail.com>
2026-05-07 23:54:21 +0500

    development

--- client/src/features/main-navigation/lib/template-helpers.ts
+++ client/src/features/main-navigation/lib/template-helpers.ts
@@ -4,6 +4,6 @@ export const isNavBtnActive = (id: string, isExactActive: boolean, routePath: st
   isExactActive || (id === 'settings' && routePath.startsWith(MAIN_PAGE_ROUTES.settings))
 
 export const getBadgeValue = (id: string, unreadInfoNotificationQuantity?: number, unreadMessagesQuantity?: number) => {
-  if (id === 'settings') return unreadInfoNotificationQuantity || undefined
+  if (id === 'info-notifications') return unreadInfoNotificationQuantity || undefined
   if (id === 'chat-rooms') return unreadMessagesQuantity || undefined
 }
