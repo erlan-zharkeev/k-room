c6018796e1f1e249cc594e89b37a2f08eb6ab64d
Erlan <zharkeev.post@gmail.com>
2026-05-10 22:44:22 +0500

    development

--- client/src/pages/contacts/ui/ContactsSearch.vue
+++ client/src/pages/contacts/ui/ContactsSearch.vue
@@ -1,63 +1,60 @@
 <script setup lang="ts">
 import { NmorphBadge, NmorphButton, NmorphIconPlusThin, NmorphScroll } from '@nmorph/nmorph-ui-kit'
-import type { InteractionType } from 'global-shared'
-import { computed, toRef, watchEffect } from 'vue'
 
-import { useSyncMedia } from 'src/entities/media-file'
 import { AppHeader, AppProfileBasicData, AppText } from 'src/shared/ui'
 
+import { CONTACTS_SEARCH_BADGE_BY_INTERACTION } from '../config/constants'
 import { CONTACTS_PAGE_I18N } from '../config/i18n'
 import type { IContactsSearchEmits, IContactsSearchProps } from '../config/types'
 import { getContactAvatarId } from '../lib/get-contact-avatar-id'
-import { useContactSearch } from '../model/use-contact-search.model'
 
 const props = defineProps<IContactsSearchProps>()
 const emit = defineEmits<IContactsSearchEmits>()
-const searchQuery = toRef(props, 'searchQuery')
-const { sync } = useSyncMedia()
-
-const { searchedContacts, searchHasMore, isSearchLoading, isSearchLoadingMore, loadMoreSearchedContacts } =
-  useContactSearch({ searchQuery })
-
-const foundContactList = computed(() => searchedContacts.value.filter(({ id }) => !props.isContactExist(id)))
-const hasSearchQuery = computed(() => Boolean(searchQuery.value))
-const showSearchResults = computed(
-  () => hasSearchQuery.value && (isSearchLoading.value || foundContactList.value.length)
-)
-const getContactBadgeText = (interactionType: InteractionType) =>
-  interactionType === 'blocked' ? CONTACTS_PAGE_I18N.blocked : CONTACTS_PAGE_I18N.invited
-
-watchEffect(() => {
-  foundContactList.value.forEach(({ id }) => sync(getContactAvatarId(id)))
-})
 </script>
 
 <template>
   <div class="contacts-search">
     <NmorphScroll>
       <div class="contacts-search__scroll-container">
-        <div v-if="showSearchResults" class="contacts-search__results">
+        <div v-if="props.showSearchResults" class="contacts-search__results">
           <AppHeader tag="h5" :text="$t(CONTACTS_PAGE_I18N.globalSearch)" />
           <AppText
-            v-if="isSearchLoading"
+            v-if="props.isSearchLoading"
             tag="small"
             color="semi-contrast-text"
             :text="$t(CONTACTS_PAGE_I18N.loading)"
           />
           <div v-else class="contacts-search__list">
             <div
-              v-for="contact in foundContactList"
+              v-for="contact in props.contactList"
               :key="contact.id"
-              class="contacts-search__item nmorph--shadow-outset"
+              class="contacts-search__item nmorph--shadow-inset"
             >
               <AppProfileBasicData
+                class="contacts-search__profile"
                 avatar-shape="circle"
                 :image-id="getContactAvatarId(contact.id)"
                 :title="contact.nickname"
                 :name="contact.nickname"
-              />
+              >
+                <template #title>
+                  <div class="contacts-search__title">
+                    <div class="contacts-search__name">
+                      <AppHeader tag="h5" truncate :text="contact.nickname" />
+                    </div>
+                    <NmorphBadge
+                      v-if="CONTACTS_SEARCH_BADGE_BY_INTERACTION[contact.interactionType].visible"
+                      class="contacts-search__status"
+                      is-tag
+                      size="tiny"
+                      :color="CONTACTS_SEARCH_BADGE_BY_INTERACTION[contact.interactionType].color"
+                      :value="$t(CONTACTS_SEARCH_BADGE_BY_INTERACTION[contact.interactionType].label)"
+                    />
+                  </div>
+                </template>
+              </AppProfileBasicData>
               <NmorphButton
-                v-if="contact.interactionType === 'default' && !props.isContactExist(contact.id)"
+                v-if="contact.interactionType === 'default'"
                 shape="square"
                 :loading="props.loadingContactIds.has(contact.id)"
                 :aria-label="$t(CONTACTS_PAGE_I18N.add)"
@@ -67,15 +64,15 @@ watchEffect(() => {
                   <NmorphIconPlusThin />
                 </template>
               </NmorphButton>
-              <NmorphBadge v-else is-tag size="tiny" :value="$t(getContactBadgeText(contact.interactionType))" />
             </div>
             <NmorphButton
-              v-if="searchHasMore"
+              v-if="props.searchHasMore"
               class="contacts-search__load-more"
-              :text="$t(CONTACTS_PAGE_I18N.loadMore)"
+              :text="$t(CONTACTS_PAGE_I18N.loadMore) + '...'"
               fill
-              :loading="isSearchLoadingMore"
-              @click="loadMoreSearchedContacts"
+              style-type="transparent"
+              :loading="props.isSearchLoadingMore"
+              @click="emit('loadMore')"
             />
           </div>
         </div>
@@ -86,6 +83,11 @@ watchEffect(() => {
 </template>
 
 <style lang="scss">
+.contacts-search__scroll-container {
+  display: grid;
+  gap: 8px;
+}
+
 .contacts-search__results {
   display: grid;
   gap: 8px;
--- client/src/shared/ui/AppProfileBasicData/AppProfileBasicData.vue
+++ client/src/shared/ui/AppProfileBasicData/AppProfileBasicData.vue
@@ -36,6 +36,7 @@ const { imageSrc } = useAppProfileBasicData(props)
 
 .app-profile-basic-data__content {
   display: flex;
+  flex: 1 1 auto;
   flex-direction: column;
   justify-content: space-between;
 
