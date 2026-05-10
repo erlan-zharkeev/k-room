2fc284de70578d75b2937ccebe59ea7ffca82241
Erlan <zharkeev.post@gmail.com>
2026-05-10 22:44:22 +0500

    development

--- client/src/pages/contacts/config/constants.ts
+++ client/src/pages/contacts/config/constants.ts
@@ -1,2 +1,35 @@
+import type { InteractionType } from 'global-shared'
+
+import { CONTACTS_PAGE_I18N } from './i18n'
+import type { IContactsSearchBadgeData } from './types'
+
 export const CONTACTS_PAGE_SEARCH_QUERY_KEY = 'search-contact'
 export const CONTACTS_PAGE_SEARCH_DEBOUNCE_MS = 300
+
+export const CONTACTS_SEARCH_BADGE_BY_INTERACTION = {
+  default: {
+    color: 'var(--nmorph-accent-color)',
+    label: CONTACTS_PAGE_I18N.invited,
+    visible: false
+  },
+  blocked: {
+    color: 'var(--nmorph-warn-color)',
+    label: CONTACTS_PAGE_I18N.blocked,
+    visible: true
+  },
+  invited: {
+    color: 'var(--nmorph-accent-color)',
+    label: CONTACTS_PAGE_I18N.invited,
+    visible: true
+  },
+  'invite-accepted': {
+    color: 'var(--nmorph-accent-color)',
+    label: CONTACTS_PAGE_I18N.invited,
+    visible: false
+  },
+  'invite-received': {
+    color: 'var(--nmorph-accent-color)',
+    label: CONTACTS_PAGE_I18N.inviteReceived,
+    visible: true
+  }
+} as const satisfies Record<InteractionType, IContactsSearchBadgeData>
