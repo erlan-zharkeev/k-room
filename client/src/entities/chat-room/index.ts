c62a896dd3bacaff2ef60395ede0967e2f1b5645
erlan_zharkeev <erlan.zharkeev@nebusfinance.ru>
2026-05-16 20:24:17 +0500

    development

--- client/src/entities/chat-room/index.ts
+++ client/src/entities/chat-room/index.ts
@@ -1,3 +1,4 @@
+export { getRoomDisplayedLastMessageId } from './lib/get-room-displayed-last-message-id'
 export { isRoomPrivate } from './lib/is-room-private'
 export { transformRoomData } from './lib/transform-room-data'
 export { useChatRoom } from './model/use-chat-room.model'
