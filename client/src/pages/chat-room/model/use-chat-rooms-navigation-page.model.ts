44fc895ea1b6d30e5ca8dae76ec6cb2d7e1d039f
erlan_zharkeev <erlan.zharkeev@nebusfinance.ru>
2026-05-18 20:59:23 +0500

    development

---
+++ client/src/pages/chat-room/model/use-chat-rooms-navigation-page.model.ts
@@ -0,0 +1,47 @@
+import { useChatRoomPreviewMessages } from './use-chat-room-preview-messages.model'
+import { useChatRoomsList } from './use-chat-rooms-list.model'
+import { useCreateChatRoomDialog } from './use-create-chat-room-dialog.model'
+
+export const useChatRoomsNavigationPage = () => {
+  const { searchQuery, chatRoomList, showNoSearchResults, showNoChats, openChatRoom } = useChatRoomsList()
+
+  const {
+    contactSearchQuery,
+    createChatName,
+    isCreateChatDialogOpen,
+    isCreatingChat,
+    selectedContactIds,
+    contactPickerList,
+    acceptedContacts,
+    isPrivateChatAlreadyExists,
+    isGroupChat,
+    canCreateChat,
+    openCreateChatDialog,
+    closeCreateChatDialog,
+    toggleContactSelection,
+    createChatRoom
+  } = useCreateChatRoomDialog(openChatRoom)
+
+  useChatRoomPreviewMessages()
+
+  return {
+    searchQuery,
+    contactSearchQuery,
+    createChatName,
+    isCreateChatDialogOpen,
+    isCreatingChat,
+    selectedContactIds,
+    chatRoomList,
+    contactPickerList,
+    acceptedContacts,
+    isPrivateChatAlreadyExists,
+    isGroupChat,
+    canCreateChat,
+    showNoSearchResults,
+    showNoChats,
+    openCreateChatDialog,
+    closeCreateChatDialog,
+    toggleContactSelection,
+    createChatRoom
+  }
+}
