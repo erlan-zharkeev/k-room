8fc425bcff99db6433c192842dfd0f6ce7088990
erlan_zharkeev <erlan.zharkeev@nebusfinance.ru>
2026-05-19 14:59:25 +0500

    development

--- client/src/pages/chat-room/model/use-chat-rooms-navigation-page.model.ts
+++ client/src/pages/chat-room/model/use-chat-rooms-navigation-page.model.ts
@@ -1,52 +1,22 @@
+import { ref } from 'vue'
+
 import { useChatRoomsList } from './use-chat-rooms-list.model'
-import { useCreateChatRoomDialog } from './use-create-chat-room-dialog.model'
 
 export const useChatRoomsNavigationPage = () => {
   const { searchQuery, chatRoomList, showNoSearchResults, showNoChats, openChatRoom } = useChatRoomsList()
+  const isCreateChatDialogOpen = ref(false)
 
-  const {
-    chatAvatarUploadKey,
-    chatAvatarUploadValue,
-    createChatNameInputValue,
-    contactSearchQuery,
-    isCreateChatDialogOpen,
-    isCreatingChat,
-    selectedContactIds,
-    acceptedContacts,
-    filteredAcceptedContacts,
-    isGroupChat,
-    canCreateChat,
-    showNoContactSearchResults,
-    openCreateChatDialog,
-    closeCreateChatDialog,
-    updateCreateChatName,
-    updateChatAvatar,
-    showUnsupportedChatAvatarFormatError,
-    createChatRoom
-  } = useCreateChatRoomDialog(openChatRoom)
+  const openCreateChatDialog = () => {
+    isCreateChatDialogOpen.value = true
+  }
 
   return {
     searchQuery,
-    chatAvatarUploadKey,
-    chatAvatarUploadValue,
-    createChatNameInputValue,
-    contactSearchQuery,
     isCreateChatDialogOpen,
-    isCreatingChat,
-    selectedContactIds,
     chatRoomList,
-    acceptedContacts,
-    filteredAcceptedContacts,
-    isGroupChat,
-    canCreateChat,
-    showNoContactSearchResults,
     showNoSearchResults,
     showNoChats,
     openCreateChatDialog,
-    closeCreateChatDialog,
-    updateCreateChatName,
-    updateChatAvatar,
-    showUnsupportedChatAvatarFormatError,
-    createChatRoom
+    openChatRoom
   }
 }
--- client/src/pages/chat-room/ui/ChatRoomsNavigationPage.vue
+++ client/src/pages/chat-room/ui/ChatRoomsNavigationPage.vue
@@ -1,41 +1,28 @@
 <script setup lang="ts">
 import {
   NmorphButton,
-  NmorphCheckbox,
-  NmorphDialog,
   NmorphIconPlusThin,
   NmorphIconSearch,
   NmorphScroll,
-  NmorphTextInput,
-  NmorphCard
+  NmorphTextInput
 } from '@nmorph/nmorph-ui-kit'
 
-import { AppProfileBasicData, AppText } from 'src/shared/ui'
+import { AppText } from 'src/shared/ui'
 
 import { CHAT_ROOM_PAGE_I18N } from '../config/i18n'
 import { useChatRoomsNavigationPage } from '../model/use-chat-rooms-navigation-page.model'
 
 import ChatRoomList from './ChatRoomList.vue'
+import CreateChatRoomDialog from './CreateChatRoomDialog.vue'
 
 const {
   searchQuery,
-  contactSearchQuery,
-  createChatName,
   isCreateChatDialogOpen,
-  isCreatingChat,
-  selectedContactIds,
   chatRoomList,
-  contactPickerList,
-  acceptedContacts,
-  isPrivateChatAlreadyExists,
-  isGroupChat,
-  canCreateChat,
   showNoSearchResults,
   showNoChats,
   openCreateChatDialog,
-  closeCreateChatDialog,
-  toggleContactSelection,
-  createChatRoom
+  openChatRoom
 } = useChatRoomsNavigationPage()
 </script>
 
@@ -60,15 +47,10 @@ const {
       </NmorphButton>
     </div>
 
-    <AppText
-      v-if="showNoChats"
-      alignment="center"
-      :selectable="false"
-      :text="$t(CHAT_ROOM_PAGE_I18N.noChats)"
-    />
+    <AppText v-if="showNoChats" alignment="center" :selectable="false" :text="$t(CHAT_ROOM_PAGE_I18N.noChats)" />
     <AppText
       v-else-if="showNoSearchResults"
-      color="semi-contrast-text"
+      alignment="center"
       :selectable="false"
       :text="$t(CHAT_ROOM_PAGE_I18N.noSearchResults)"
     />
@@ -76,93 +58,7 @@ const {
       <ChatRoomList :items="chatRoomList" />
     </NmorphScroll>
 
-    <NmorphDialog
-      :model-value="isCreateChatDialogOpen"
-      :title="$t(CHAT_ROOM_PAGE_I18N.createChatTitle)"
-      @update:model-value="($event) => ($event ? openCreateChatDialog() : closeCreateChatDialog())"
-    >
-      <div class="create-chat-room-dialog">
-        <NmorphTextInput
-          v-if="isGroupChat"
-          v-model.trim="createChatName"
-          :placeholder="$t(CHAT_ROOM_PAGE_I18N.chatName)"
-          :input-attrs="{ 'aria-label': $t(CHAT_ROOM_PAGE_I18N.chatName) }"
-        />
-        <NmorphTextInput
-          v-model="contactSearchQuery"
-          clearable
-          :placeholder="$t(CHAT_ROOM_PAGE_I18N.contactSearch)"
-          :input-attrs="{ 'aria-label': $t(CHAT_ROOM_PAGE_I18N.contactSearch) }"
-        >
-          <template #prepend-icon>
-            <NmorphIconSearch />
-          </template>
-        </NmorphTextInput>
-        <div class="create-chat-room-dialog__meta">
-          <AppText
-            tag="small"
-            color="semi-contrast-text"
-            :selectable="false"
-            :text="$t(CHAT_ROOM_PAGE_I18N.contacts)"
-          />
-          <AppText
-            tag="small"
-            color="semi-contrast-text"
-            :selectable="false"
-            :text="`${$t(CHAT_ROOM_PAGE_I18N.selectedContacts)}: ${selectedContactIds.length}`"
-          />
-        </div>
-        <AppText
-          v-if="isPrivateChatAlreadyExists"
-          color="warn"
-          :selectable="false"
-          :text="$t(CHAT_ROOM_PAGE_I18N.privateChatExists)"
-        />
-        <AppText
-          v-if="!acceptedContacts.length"
-          color="semi-contrast-text"
-          :selectable="false"
-          :text="$t(CHAT_ROOM_PAGE_I18N.noContacts)"
-        />
-        <NmorphScroll v-else scroll-x-prop="hidden" class="create-chat-room-dialog__contacts">
-          <div class="create-chat-room-dialog__contact-list">
-            <NmorphCard
-              shadow-type="inset"
-              tag="label"
-              v-for="contact in contactPickerList"
-              :key="contact.id"
-              class="create-chat-room-dialog__contact"
-            >
-              <AppProfileBasicData
-                class="create-chat-room-dialog__profile"
-                :image-id="contact.imageId"
-                :title="contact.title"
-                :name="contact.title"
-                :selectable="false"
-              />
-              <NmorphCheckbox
-                :model-value="contact.selected"
-                @update:model-value="toggleContactSelection(contact.id, $event)"
-              />
-            </NmorphCard>
-          </div>
-        </NmorphScroll>
-        <div class="create-chat-room-dialog__actions">
-          <NmorphButton
-            style-type="transparent"
-            :text="$t(CHAT_ROOM_PAGE_I18N.cancel)"
-            :disabled="isCreatingChat"
-            @click="closeCreateChatDialog"
-          />
-          <NmorphButton
-            :text="$t(CHAT_ROOM_PAGE_I18N.createChat)"
-            :loading="isCreatingChat"
-            :disabled="!canCreateChat"
-            @click="createChatRoom"
-          />
-        </div>
-      </div>
-    </NmorphDialog>
+    <CreateChatRoomDialog v-model="isCreateChatDialogOpen" @created="openChatRoom" />
   </section>
 </template>
 
