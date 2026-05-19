6407e3252e85d326030e7ca23a658b44e4f45fba
erlan_zharkeev <erlan.zharkeev@nebusfinance.ru>
2026-05-19 11:29:02 +0500

    development

--- client/src/entities/setting/model/use-localized-date-time.model.ts
+++ client/src/entities/setting/model/use-localized-date-time.model.ts
@@ -11,25 +11,13 @@ export const useLocalizedDateTime = () => {
   const { settings } = useSettings()
 
   const formatDate = (value: number | string) =>
-    formatLocalizedDate(
-      value,
-      settings.value.localization.language,
-      settings.value.localization.dateTimeFormat
-    )
+    formatLocalizedDate(value, settings.value.localization.language, settings.value.localization.dateTimeFormat)
 
   const formatTime = (value: number | string) =>
-    formatLocalizedTime(
-      value,
-      settings.value.localization.language,
-      settings.value.localization.dateTimeFormat
-    )
+    formatLocalizedTime(value, settings.value.localization.language, settings.value.localization.dateTimeFormat)
 
   const formatDateTime = (value: number | string) =>
-    formatLocalizedDateTime(
-      value,
-      settings.value.localization.language,
-      settings.value.localization.dateTimeFormat
-    )
+    formatLocalizedDateTime(value, settings.value.localization.language, settings.value.localization.dateTimeFormat)
 
   const formatRelativeTime = (value: number | string) =>
     formatLocalizedRelativeTime(value, settings.value.localization.language)
