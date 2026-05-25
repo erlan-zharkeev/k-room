6f90814cf7647558fe46255be5c42db3881829bd
erlan_zharkeev <erlan.zharkeev@nebusfinance.ru>
2026-05-25 17:18:21 +0500

    devleopment

--- client/src/features/content-navigation-back-button/ui/ContentNavigationBackButton.vue
+++ client/src/features/content-navigation-back-button/ui/ContentNavigationBackButton.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { NmorphButton, NmorphCard, NmorphIcon, NmorphIconBack } from '@nmorph/nmorph-ui-kit'
+import { NmorphButton } from '@nmorph/nmorph-ui-kit'
 
 import { CONTENT_NAVIGATION_BACK_BUTTON_I18N } from '../config/constants'
 import { useContentNavigationBackButton } from '../model/use-content-navigation-back-button.model'
@@ -8,18 +8,11 @@ const { navigateToContentNavigation } = useContentNavigationBackButton()
 </script>
 
 <template>
-  <NmorphCard shadow-type="inset" content-class="content-navigation-back-button">
-    <NmorphButton :aria-label="$t(CONTENT_NAVIGATION_BACK_BUTTON_I18N.back)" @click="navigateToContentNavigation">
-      <NmorphIcon width="16px">
-        <NmorphIconBack />
-      </NmorphIcon>
-    </NmorphButton>
-  </NmorphCard>
+  <NmorphButton
+    style-type="transparent"
+    color="var(--nmorph-contrast-text-color)"
+    :text="$t(CONTENT_NAVIGATION_BACK_BUTTON_I18N.back)"
+    :aria-label="$t(CONTENT_NAVIGATION_BACK_BUTTON_I18N.back)"
+    @click="navigateToContentNavigation"
+  />
 </template>
-
-<style lang="scss">
-.content-navigation-back-button {
-  display: flex;
-  align-items: center;
-}
-</style>
