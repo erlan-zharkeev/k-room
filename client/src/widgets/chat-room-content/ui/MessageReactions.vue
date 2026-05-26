29afee30f9c7bde0ee1c7ee3bb73168fc0d657ad
erlan_zharkeev <erlan.zharkeev@nebusfinance.ru>
2026-05-26 22:37:45 +0500

    development

---
+++ client/src/widgets/chat-room-content/ui/MessageReactions.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { NmorphTagList } from '@nmorph/nmorph-ui-kit'
+
+import type { MessageReactionsProps } from '../config/types'
+import { useMessageReactions } from '../model/use-message-reactions.model'
+
+const props = defineProps<MessageReactionsProps>()
+const { reactionTagList, reactionTagListKey } = useMessageReactions(props)
+</script>
+
+<template>
+  <NmorphTagList
+    v-if="reactionTagList.length"
+    :key="reactionTagListKey"
+    class="message-reactions"
+    :model-value="reactionTagList"
+  />
+</template>
