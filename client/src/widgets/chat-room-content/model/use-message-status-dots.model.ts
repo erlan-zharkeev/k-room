87e61d901d9cf9542d63e4c27903e18c7c951d54
erlan_zharkeev <erlan.zharkeev@nebusfinance.ru>
2026-05-28 00:29:07 +0500

    development

---
+++ client/src/widgets/chat-room-content/model/use-message-status-dots.model.ts
@@ -0,0 +1,21 @@
+import { MESSAGE_STATUS_VALUE } from 'global-shared'
+import { computed, type Ref } from 'vue'
+
+import type { MessageRecord } from 'src/shared/lib'
+
+import { MESSAGE_STATUS_DOT_COUNT_BY_STATUS, MESSAGE_STATUS_DOT_TONE_BY_STATUS } from '../config/constants'
+
+export const useMessageStatusDots = (message: Ref<MessageRecord>) => {
+  const status = computed(() => message.value.status ?? MESSAGE_STATUS_VALUE.NONE)
+  const dotCount = computed(() => MESSAGE_STATUS_DOT_COUNT_BY_STATUS[status.value])
+  const tone = computed(() => MESSAGE_STATUS_DOT_TONE_BY_STATUS[status.value])
+  const isBlinking = computed(() => status.value === MESSAGE_STATUS_VALUE.SENDING)
+  const isVisible = computed(() => Boolean(message.value.isSelf) && dotCount.value > 0)
+
+  return {
+    dotCount,
+    isBlinking,
+    isVisible,
+    tone
+  }
+}
