ab82e2fbd2f3ce8de29b92d51c55409d4be445b6
erlan_zharkeev <erlan.zharkeev@nebusfinance.ru>
2026-05-31 13:36:52 +0500

    development

--- client/src/entities/media-file/index.ts
+++ client/src/entities/media-file/index.ts
@@ -1,4 +1,8 @@
-export { MESSAGE_IMAGE_DRAFT_MEDIA_ID_PREFIX, UPDATE_MEDIA_INTERVAL_MS } from './config/constants'
+export {
+  MESSAGE_DOCUMENT_DRAFT_MEDIA_ID_PREFIX,
+  MESSAGE_IMAGE_DRAFT_MEDIA_ID_PREFIX,
+  UPDATE_MEDIA_INTERVAL_MS
+} from './config/constants'
 export { acquireUrl, releaseUrl, useLiveMediaUrl } from 'src/shared/lib'
 export { syncMedia } from './lib/sync-media'
 export { transformHeadersToMediaData } from './lib/transform-headers-to-media-data'
