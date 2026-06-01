3c85b0366c2e4c6861e91e68ebc7fbd9a0a41b3e
erlan_zharkeev <erlan.zharkeev@nebusfinance.ru>
2026-06-01 22:26:20 +0500

    development

--- client/src/features/room-call-session/index.ts
+++ client/src/features/room-call-session/index.ts
@@ -1,2 +1,3 @@
+export { useActiveRoomCallSession } from './model/use-active-room-call-session.model'
 export { useRoomCallLocalMedia } from './model/use-room-call-local-media.model'
 export { useRoomCallSession } from './model/use-room-call-session.model'
