034117af40dcf6ad7f212d3f2ad5ae9f4a2aeba7
erlan_zharkeev <erlan.zharkeev@nebusfinance.ru>
2026-06-03 10:47:33 +0500

    development

---
+++ client/src/features/room-call-session/ui/RoomCallAudioContextMenuItem.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { NmorphIcon, NmorphIconPhone } from '@nmorph/nmorph-ui-kit'
+
+import { AppText } from 'src/shared/ui'
+
+import { ROOM_CALL_SESSION_I18N } from '../config/i18n'
+import type { RoomCallAudioContextMenuItemEmits, RoomCallAudioContextMenuItemProps } from '../config/types'
+import { useRoomCallAudioContextMenuItem } from '../model/use-room-call-audio-context-menu-item.model'
+
+const props = defineProps<RoomCallAudioContextMenuItemProps>()
+const emit = defineEmits<RoomCallAudioContextMenuItemEmits>()
+const { isRoomCallAudioContextMenuItemDisabled, startAudioRoomCall } = useRoomCallAudioContextMenuItem(props, emit)
+</script>
+
+<template>
+  <button
+    class="room-call-audio-context-menu-item"
+    type="button"
+    :aria-disabled="isRoomCallAudioContextMenuItemDisabled"
+    @click.stop="startAudioRoomCall"
+  >
+    <AppText :selectable="false" :text="$t(ROOM_CALL_SESSION_I18N.startAudioRoomCall)" />
+  </button>
+</template>
+
+<style lang="scss">
+.room-call-audio-context-menu-item {
+  padding: 0;
+  font: inherit;
+}
+
+.room-call-audio-context-menu-item[aria-disabled='true'] {
+  @include disabled-state;
+}
+</style>
---
+++ client/src/features/room-call-session/ui/RoomCallStartButtons.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { NmorphButton, NmorphIcon, NmorphDivider } from '@nmorph/nmorph-ui-kit'
+
+import { ROOM_CALL_START_BUTTONS } from '../config/constants'
+import type { RoomCallStartButtonsEmits, RoomCallStartButtonsProps } from '../config/types'
+
+const props = defineProps<RoomCallStartButtonsProps>()
+const emit = defineEmits<RoomCallStartButtonsEmits>()
+</script>
+
+<template>
+  <div class="room-call-start-buttons">
+    <NmorphButton
+      v-for="button in ROOM_CALL_START_BUTTONS"
+      :key="button.mediaKind"
+      shape="square"
+      style-type="transparent"
+      :aria-label="$t(button.ariaLabel)"
+      :title="$t(button.ariaLabel)"
+      :disabled="props.disabled"
+      :loading="props.loading"
+      @click="emit('start', button.mediaKind)"
+    >
+      <NmorphIcon width="16px" height="16px">
+        <component :is="button.icon" />
+      </NmorphIcon>
+    </NmorphButton>
+    <NmorphDivider direction="vertical" />
+  </div>
+</template>
+
+<style lang="scss">
+.room-call-start-buttons {
+  display: flex;
+  gap: 8px;
+  align-items: center;
+}
+</style>
